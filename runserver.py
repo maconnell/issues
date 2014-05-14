943fddde0ff18d3a15142ccf4a14cc8fc396e3f4
Mike Connell <michael.connell@gmail.com>
2014-05-14 15:19:28 +0200

    some setup help, file upload still TBD

--- runserver.py
+++ runserver.py
@@ -1,3 +1,11 @@
 from issues import app
 
-app.run(debug=True)
+"""
+Installing, first run
+1. setup virtual env (TODO)
+2. run db.createDB, resulting issues.db should be located in head directory (besides this file)
+3. copy default section from issues/__init__.py to ~/.issues.cfg and edit to taste
+4. login is as admin/admin and change password!
+"""
+
+app.run(debug=True,host='0.0.0.0',port=8080)
